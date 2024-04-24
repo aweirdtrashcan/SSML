ab26c60435d0be051e311d40c209b0a751869f8d
Diego Rodrigues <diegodrp01@gmail.com>
2024-04-24 17:04:18 -0300

    vector 2 added

--- SSML/Vector2d.hpp
+++ SSML/Vector2d.hpp
@@ -2,5 +2,57 @@
 
 #include <nmmintrin.h>
 
+namespace SSML {
+	struct VECTOR2D {
+		union {
+			struct {
+				float x;
+				float y;
+			};
+			struct {
+				float u;
+				float v;
+			};
+			struct {
+				float r;
+				float g;
+			};
+		};
 
+		VECTOR2D operator*(const VECTOR2D& other) {
+			VECTOR2D v;
+			v.x = this->x * other.x;
+			v.y = this->y * other.y;
+			return v;
+		}
+	};
+
+	struct alignas(16) XMMVECTOR {
+		__m128 m_Data;
+	
+		XMMVECTOR() = default;
+		~XMMVECTOR() = default;
+
+		XMMVECTOR(const VECTOR2D& vector) {
+			float m4d[4];
+			m4d[0] = vector.r;
+			m4d[1] = vector.g;
+			m4d[2] = 0.0f;
+			m4d[3] = 0.0f;
+			m_Data = _mm_load_ps(m4d);
+		}
+
+		XMMVECTOR(__m128 d) {
+			m_Data = d;
+		}
+
+		XMMVECTOR operator*(const XMMVECTOR other) {
+			return _mm_mul_ps(this->m_Data, other.m_Data);
+		}
+
+		void GetFloatArray(float* out_float_array) {
+			_mm_store_ps(out_float_array, m_Data);
+		}
+	};
+}
 
--- SSML/main.cpp
+++ SSML/main.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
 
+#include "Vector2d.hpp"
+
+#include <chrono>
+
 int main() {
-	
+	SSML::VECTOR2D v = { 3.5984f, 4.3890f };
+	SSML::XMMVECTOR xmv = v;
+
+	auto n0 = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < 30000; i++)
+		SSML::VECTOR2D v1 = v * v;
+	auto end0 = std::chrono::high_resolution_clock::now();
+
+	auto n1 = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < 30000; i++) SSML::XMMVECTOR xmv1 = xmv * xmv;
+	auto end1 = std::chrono::high_resolution_clock::now();
+
+	auto start0 = std::chrono::time_point_cast<std::chrono::microseconds>(n0).time_since_epoch().count();
+	auto mend0 = std::chrono::time_point_cast<std::chrono::microseconds>(end0).time_since_epoch().count();
+
+	auto start1 = std::chrono::time_point_cast<std::chrono::microseconds>(n1).time_since_epoch().count();
+	auto mend1 = std::chrono::time_point_cast<std::chrono::microseconds>(end1).time_since_epoch().count();
+
+	auto duration0 = mend0 - start0;
+	auto duration1 = mend1 - start1;
+
+	double percentage = ((double)duration0 / (double)duration1) * 100 - 100;
+
+	std::cout << "Non SIMD took " << duration0 << " time\n";
+	std::cout << "SIMD took " << duration1 << " time\n";
+	std::cout << "SIMD was " << percentage << "% faster than non SIMD\n";
+
+	//std::cout << v1.x << " | " << v1.y << "\n";
+
+	//float fa[4];
+	//xmv1.GetFloatArray(fa);
+
+	//std::cout << fa[0] << " | " << fa[1] << "\n";
 }
